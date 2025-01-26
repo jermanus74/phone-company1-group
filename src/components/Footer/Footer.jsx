d99b794b6baf5e2e19cf08635f98b4e2cf275853
Jermanus peter <133773488+jermanus74@users.noreply.github.com>
2025-01-26 21:27:56 +0300

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import {
   FaFacebook,
@@ -17,78 +18,115 @@ export const Footer = () => {
 
   return (
     <div className="footer dark:bg-gray-950 text-white">
-      <div className="container">
-        <div className="grid md:grid-cols-5 pb-20 pt-5">
+      <div className="container mx-auto px-4">
+        <div className="flex justify-evenly pb-20 pt-5">
           {/* Company details */}
-          <div className="footer-company-details py-8 px-4">
-            <a href="/" className="footer-logo text-brandBlue font-helvetica">
+          <div className="footer-company-details py-8 px-4 sm:text-center">
+            <a
+              href="#"
+              className="footer-logo text-brandBlue font-helvetica text-3xl font-bold"
+            >
               samsung
             </a>
-            <p className="footer-description text-gray-600">
+            <p className="footer-description text-gray-400 mt-4">
               Lorem ipsum dolor sit amet consectetur adipisicing elit.
             </p>
-            <p className="footer-credits">By MUST Computer Science Students</p>
-            <a href="/" className="visit-button bg-brandBlue text-white">
+            <p className="footer-credits mt-6 text-gray-500">
+              By MUST Computer Science Students
+            </p>
+            <a
+              href="#"
+              className="visit-button mt-6 inline-block bg-brandBlue text-white py-2 px-6 rounded-lg"
+            >
               Visit Our Website
             </a>
           </div>
 
           {/* Footer links */}
-          <div className="footer-links col-span-2 grid grid-cols-2 sm:grid-cols-3 md:pl-5">
+          <div
+            className="footer-links grid grid-cols-1
+     sm:grid-cols-2 md:grid-cols-1 lg:col-span-2 
+     py-8 px-4 space-y-6 sm:text-center sm:justify-center"
+          >
             {/* First set of links */}
-            <div className="footer-link-group py-8 px-4 mr-40 w-max">
-              <h1 className="footer-link-title">Important Links</h1>
-              <ul className="footer-link-list">
+            <div className="footer-link-group">
+              <h1 className="footer-link-title text-xl font-semibold text-gray-300">
+                Important Links
+              </h1>
+              <ul className="footer-link-list space-y-3 mt-4">
                 {FooterLinks.map((data, index) => (
                   <li key={index}>
-                    <a href={data.link} className="footer-link">
+                    <a
+                      href={data.link}
+                      className="footer-link text-gray-400 hover:text-white"
+                    >
                       {data.title}
                     </a>
                   </li>
                 ))}
               </ul>
             </div>
-
-            {/* Second set of links */}
-            <div className="footer-link-group  py-8 space-x-5 px-4 w-max">
-              <h1 className="footer-link-title m-5">Important Links</h1>
-              <ul className="footer-link-list">
-                {FooterLinks.map((data, index) => (
-                  <li key={index}>
-                    <a href={data.link} className="footer-link">
-                      {data.title}
-                    </a>
-                  </li>
-                ))}
-              </ul>
+          </div>
+          {/* Second set of links */}
+          <div className="footer-company-details py-8 px-4 sm:text-center">
+            <h1 className="footer-link-title text-xl font-semibold text-gray-300">
+              More Links
+            </h1>
+            <ul className="footer-link-list space-y-3 mt-4">
+              {FooterLinks.map((data, index) => (
+                <li key={index}>
+                  <a
+                    href={data.link}
+                    className="footer-link text-gray-400 hover:text-white"
+                  >
+                    {data.title}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+          {/* Company address and social links */}
+          <div className="footer-contact py-8 px-4 col-span-1 sm:col-span-1 lg:col-span-1 sm:text-center sm:items-center sm:justify-center">
+            <h1 className="text-xl font-semibold text-gray-300">Address: </h1>
+            <div className="contact-item flex items-center space-x-3 mt-4 sm:justify-center">
+              <FaLocationArrow className="text-brandBlue" />
+              <p className="text-gray-400">Mbezi, Dar-es-Salaam, Tanzania</p>
             </div>
-
-            {/* Company address and social links */}
-            <div className="footer-contact ml-40 py-8 px-4 col-span-2 sm:col-auto w-max">
-              <div className="">
-                <div className="contact-item">
-                  <FaLocationArrow />
-                  <p>Mbezi, Dar-es-Salaam, Tanzania</p>
-                </div>
-                <div className="contact-item mt-6">
-                  <FaMobileAlt />
-                  <p>+255 746-342-550</p>
-                </div>
-              </div>
-              <div className="footer-social-links mt-6">
-                <a href="/">
-                  <FaInstagram className="social-icon" />
-                </a>
-                <a href="/">
-                  <FaFacebook className="social-icon" />
-                </a>
-                <a href="/">
-                  <FaLinkedin className="social-icon" />
-                </a>
-              </div>
+            <div className="contact-item flex items-center space-x-3 mt-6 sm:justify-center">
+              <FaMobileAlt className="text-brandBlue" />
+              <p className="text-gray-400">+255 746-342-550</p>
+            </div>
+            <div className="footer-social-links mt-6 flex space-x-4 sm:justify-center">
+              <a
+                href="#"
+                className="social-icon text-brandBlue hover:text-white"
+              >
+                <FaInstagram />
+              </a>
+              <a
+                href="#"
+                className="social-icon text-brandBlue hover:text-white"
+              >
+                <FaFacebook />
+              </a>
+              <a
+                href="#"
+                className="social-icon text-brandBlue hover:text-white"
+              >
+                <FaLinkedin />
+              </a>
             </div>
           </div>
         </div>
+      </div>
+      <div className="flex justify-center bg-gray-800 p-2 space-x-10 text-white">
+        <span className="font-bold  decoration-blue-500">Future Tech</span>
+        <span className="italic text-gray-400">Innovative Creations</span>
+        <span className="font-semibold decoration-blue-500">Samsung </span>
+        <span className="uppercase tracking-wide decoration-green-500 ">
+          NextGen Systems
+        </span>
+        <span>© 2025</span>
       </div>
     </div>
   );
